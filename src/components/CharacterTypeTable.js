b6877c3fb20c21f1bbb08f0d28aa30aa5e1a7c6e
godmar02 <markwgodden@gmail.com>
2021-02-22 21:18:10 +0000

    dynamic drop downs

--- src/components/CharacterTypeTable.js
+++ src/components/CharacterTypeTable.js
@@ -1,52 +1,68 @@
 import React from 'react';
+import {races} from '../data/raceList'
+import {dwClasses} from '../data/classList'
 
 function CharacterTypeTable() {
 
-  return (
-    <table style={{
-        "width" : "100%"
-      }} id="additionalInfoTable">
-      <tbody>
-        <tr>
-          <th>
-            <label style={{
-                "width" : "25%"
-              }} htmlFor="dwClass">CLASS</label>
-          </th>
-          <td style={{
+  return (<table style={{
+      "width" : "100%"
+    }} id="additionalInfoTable">
+    <tbody>
+      <tr>
+        <th>
+          <label style={{
               "width" : "25%"
-            }}>
-            <select tabIndex={-1} id="dwClass"/>
-          </td>
-          <td style={{
-              "width" : "50%"
-            }}/>
-        </tr>
-        <tr>
-          <th>
-            <label htmlFor="race">RACE</label>
-          </th>
-          <td>
-            <select tabIndex={-1} id="race"/>
-          </td>
-          <td>
-            <textarea className="grey" readOnly="readOnly" id="raceAttribute" defaultValue={""}/>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <label htmlFor="alignment">ALIGNMENT</label>
-          </th>
-          <td>
-            <select tabIndex={-1} id="alignment"/>
-          </td>
-          <td>
-            <textarea className="grey" readOnly="readOnly" id="alignmentAttribute" defaultValue={""}/>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    );
+            }} htmlFor="dwClass">CLASS</label>
+        </th>
+        <td style={{
+            "width" : "25%"
+          }}>
+          <select tabIndex={-1} id="dwClass">
+            {
+              dwClasses.map((data) => {
+                return (<option>
+                  {data}
+                </option>);
+              })
+            }
+            </select>
+        </td>
+        <td style={{
+            "width" : "50%"
+          }}/>
+      </tr>
+      <tr>
+        <th>
+          <label htmlFor="race">RACE</label>
+        </th>
+        <td>
+          <select tabIndex={-1} id="race">
+          {
+            races.map((data, key) => {
+              return (<option>
+                {data}
+              </option>);
+            })
+          }
+          </select>
+        </td>
+        <td>
+          <textarea className="grey" readOnly="readOnly" id="raceAttribute" defaultValue={""}/>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label htmlFor="alignment">ALIGNMENT</label>
+        </th>
+        <td>
+          <select tabIndex={-1} id="alignment"/>
+        </td>
+        <td>
+          <textarea className="grey" readOnly="readOnly" id="alignmentAttribute" defaultValue={""}/>
+        </td>
+      </tr>
+    </tbody>
+  </table>);
 }
 
 export default CharacterTypeTable;
