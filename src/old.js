93c1a32d738b3a0da7a11a2ae3fca54313e39679
godmar02 <markwgodden@gmail.com>
2021-03-01 17:04:15 +0000

    removing more old code

--- src/old.js
+++ src/old.js
@@ -1,11 +1,11 @@
   function singleRoll(sides) {
-    var roll = Math.floor(Math.random() * sides) + 1;
+    const roll = Math.floor(Math.random() * sides) + 1;
     return roll;
   }
 
   function rollDice(sides, number) {
-    var total = 0;
-    for (var i = 1; i == number; i++) {
+    const total = 0;
+    for (const i = 1; i == number; i++) {
       total = total + singleRoll(sides);
     }
     return total;
@@ -16,12 +16,6 @@
     var maxLoad = parseInt($("#maxLoad").val().replace("/ ", ""), 10);
     if (load && maxLoad && load > maxLoad) {
       alert("validateLoad() - " + load + " weight exceeds maximum permitted value of " + maxLoad);
-      loadErrors = true;
-    } else {
-      loadErrors = false;
-    }
-    if (debug == true) {
-      console.info("validateLoad() - loadErrors:", loadErrors);
     }
   }
 
@@ -35,26 +29,8 @@
     var maxAbility = 73; //16, 15, 13, 12, 9, 8
     var totalAbility = str + dex + con + int + wis + cha;
 
-    if (debug == true) {
-      console.info("validateAbilityScore() - str:", str);
-      console.info("validateAbilityScore() - dex:", dex);
-      console.info("validateAbilityScore() - con:", con);
-      console.info("validateAbilityScore() - int:", int);
-      console.info("validateAbilityScore() - wis:", wis);
-      console.info("validateAbilityScore() - cha:", cha);
-      console.info("validateAbilityScore() - maxAbility:", maxAbility);
-      console.info("validateAbilityScore() - totalAbility:", totalAbility);
-    }
-
     if (str && dex && con && int && wis && cha && totalAbility != maxAbility) {
       alert("validateAbilityScore() - " + totalAbility + " Ability Score does not match total permitted value of " + maxAbility + "\n" + "Suggested values are 16, 15, 13, 12, 9, 8");
-      abilityErrors = true;
-    } else {
-      abilityErrors = false;
-    }
-
-    if (debug == true) {
-      console.info("validateAbilityScore() - abilityErrors:", abilityErrors);
     }
   }
 
@@ -63,7 +39,6 @@
     var maxXp = parseInt($("#maxXp").val().replace("/ ", ""), 10);
     if (xp && maxXp && xp > maxXp) {
       alert("validateXp() - " + xp + "xp exceeds maximum permitted value of " + maxXp);
-      $("#xp").val(maxXp);
     }
   }
 
@@ -72,6 +47,5 @@
     var maxHp = parseInt($("#maxHp").val().replace("/ ", ""), 10);
     if (hp && maxHp && hp > maxHp) {
       alert("validateHp() - " + hp + "hp exceeds maximum permitted value of " + maxHp);
-      $("#hp").val(maxHp);
     }
   }
