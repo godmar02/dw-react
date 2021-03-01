fa4bfdff4df73f333304d471eb0aa0c46777b719
godmar02 <markwgodden@gmail.com>
2021-03-01 12:28:29 +0000

    custom styling for logout

--- src/components/profile/ProfileDetails.js
+++ src/components/profile/ProfileDetails.js
@@ -4,6 +4,7 @@ import * as FirebaseService from 'services/firebase';
 import ProfileState from 'components/contexts/ProfileState';
 import {Button} from '@material-ui/core';
 
+
 function ProfileDetails() {
 
   const [currentUser] = useContext(AuthState);
@@ -11,8 +12,23 @@ function ProfileDetails() {
   const toggleSetShow = () => setShow(!show);
 
     return (
-      <div>
-        <div>Google Account</div>
+      <div style={{"border": "0",
+      "-webkit-border-radius": "50%",
+      "border-radius": "5px",
+      "display": "inline-block",
+      "margin": "0",
+      "position": "absolute",
+      "top": "50px",
+      "right": "20px",
+      "color": "#bdc1c6",
+      "background-color": "rgba(60, 64, 67, 0.9)",
+      "text-align": "left",
+      "font": "500 12px 'Roboto', arial, sans-serif",
+      "letter-spacing": "0.8px",
+      "line-height": "16px",
+      "font-weight": "bold",
+      "padding": "8px 8px"}}>
+        <div style={{"color": "white"}}>Google Account</div>
         <div>{currentUser.displayName}</div>
         <div>{currentUser.email}</div>
         <Button variant="contained" color="primary" onClick={() => FirebaseService.auth.signOut()}>LOG OUT</Button>
