ee942b95b0ff545a3c36833737ee4317166b13dd
MarkG <markgodden@uk.ibm.com>
2021-03-03 09:26:03 +0000

    removed old dice code

--- src/old.js
+++ src/old.js
@@ -1,9 +1,14 @@
-
   function validateLoad() {
     var load = parseInt($("#load").val(), 10);
     var maxLoad = parseInt($("#maxLoad").val().replace("/ ", ""), 10);
     if (load && maxLoad && load > maxLoad) {
       alert("validateLoad() - " + load + " weight exceeds maximum permitted value of " + maxLoad);
+      loadErrors = true;
+    } else {
+      loadErrors = false;
+    }
+    if (debug == true) {
+      console.info("validateLoad() - loadErrors:", loadErrors);
     }
   }
 
@@ -17,8 +22,26 @@
     var maxAbility = 73; //16, 15, 13, 12, 9, 8
     var totalAbility = str + dex + con + int + wis + cha;
 
+    if (debug == true) {
+      console.info("validateAbilityScore() - str:", str);
+      console.info("validateAbilityScore() - dex:", dex);
+      console.info("validateAbilityScore() - con:", con);
+      console.info("validateAbilityScore() - int:", int);
+      console.info("validateAbilityScore() - wis:", wis);
+      console.info("validateAbilityScore() - cha:", cha);
+      console.info("validateAbilityScore() - maxAbility:", maxAbility);
+      console.info("validateAbilityScore() - totalAbility:", totalAbility);
+    }
+
     if (str && dex && con && int && wis && cha && totalAbility != maxAbility) {
       alert("validateAbilityScore() - " + totalAbility + " Ability Score does not match total permitted value of " + maxAbility + "\n" + "Suggested values are 16, 15, 13, 12, 9, 8");
+      abilityErrors = true;
+    } else {
+      abilityErrors = false;
+    }
+
+    if (debug == true) {
+      console.info("validateAbilityScore() - abilityErrors:", abilityErrors);
     }
   }
 
@@ -27,6 +50,7 @@
     var maxXp = parseInt($("#maxXp").val().replace("/ ", ""), 10);
     if (xp && maxXp && xp > maxXp) {
       alert("validateXp() - " + xp + "xp exceeds maximum permitted value of " + maxXp);
+      $("#xp").val(maxXp);
     }
   }
 
@@ -35,5 +59,6 @@
     var maxHp = parseInt($("#maxHp").val().replace("/ ", ""), 10);
     if (hp && maxHp && hp > maxHp) {
       alert("validateHp() - " + hp + "hp exceeds maximum permitted value of " + maxHp);
+      $("#hp").val(maxHp);
     }
   }
