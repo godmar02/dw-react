d5e87e8b6aac5ab3be3ec45303758b1d8bb48ae8
godmar02 <markwgodden@gmail.com>
2021-03-24 22:33:17 +0000

    version removed

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -41,7 +41,7 @@ export default function AppHeader() {
             <Menu />
           </IconButton>
           <Typography component={'span'} variant='h6' className={classes.title}>
-            Dungeon World Character Sheet (v0.10.0 by Godmar02)
+            Dungeon World Character Sheet (by Godmar02)
           </Typography>
           <Switch checked={darkState} onChange={handleThemeChange} />
           <UserProfile />
